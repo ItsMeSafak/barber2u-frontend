432e37d70a45c7722c60b80bc6d69bb61cce9e8b
Mehmet Tetik <mehmet.tetik@hva.nl>
2021-03-02 23:09:45 +0100

    Add header/hamburger menu/footer/font family.

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,32 @@
 import React from "react";
-import logo from "./logo.svg";
-import Header from "./template/header";
 
+import Header from "./template/header";
+import Footer from "./template/footer";
+
+import logo from "./logo.svg";
 import "./App.scss";
 
-const App: React.FC = () => {
-    const navbarElements = [
-        {
-            url: "/home",
-            name: "Home",
-        },
-        {
-            url: "/how-it-works",
-            name: "How it works",
-        },
-        {
-            url: "/about",
-            name: "About",
-        },
-        {
-            url: "/contact",
-            name: "Contact",
-        },
-        {
-            url: "/login",
-            name: "Log in",
-            isPillButton: true,
-        },
-    ];
-
-    return (
-        <>
-            <Header tabs={navbarElements} />
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        </>
-    );
-};
+const App: React.FC = () => (
+    <>
+        <Header />
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+        <Footer />
+    </>
+);
 
 export default App;
