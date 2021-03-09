37913d2dcbb91f98c83d2c4b1c6bad06cf0c30bc
Safak Inan <safak.inan61@hotmail.com>
2021-03-09 21:34:47 +0100

    new sidebar

--- src/template/sidebar/index.tsx
+++ src/template/sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconName, IconProp } from "@fortawesome/fontawesome-svg-core";
@@ -6,10 +6,17 @@ import {
     faCog,
     faCalendar,
     faCaretRight,
+    faChartPie,
+    faCalendarAlt,
+    faCamera,
+    faCut,
 } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
+import Sider from "antd/lib/layout/Sider";
+import { Layout, Menu } from "antd";
 
 import styles from "./styles.module.scss";
+
 
 interface ComponentProps {
     baseUrl?: string;
@@ -22,8 +29,18 @@ interface ComponentProps {
 }
 
 const Sidebar: React.FC<ComponentProps> = ({ baseUrl, isMobile, items }) => {
+    const [currentPath, setCurrentPath] = useState(`/${window.location.pathname.split("/")[2]}`);
+
     const stringToIcon: any = (iconName: string) => {
         switch (iconName) {
+            case "faChartPie":
+                return <FontAwesomeIcon icon={faChartPie} />;
+            case "faCalendarAlt":
+                return <FontAwesomeIcon icon={faCalendarAlt} />;
+            case "faCamera":
+                return <FontAwesomeIcon icon={faCamera} />;
+            case "faCut":
+                return <FontAwesomeIcon icon={faCut} />;
             case "faCog":
                 return <FontAwesomeIcon icon={faCog} />;
             case "faCalendar":
@@ -37,26 +54,27 @@ const Sidebar: React.FC<ComponentProps> = ({ baseUrl, isMobile, items }) => {
 
     return (
         <>
-            <div
-                className={`${styles.sidebar} ${
-                    isMobile ? styles.sidebarMobile : styles.sideBarDesktop
-                }`}
-            >
-                <ul>
+            <Layout
+             className={`${styles.side} ${
+                isMobile ? styles.sidebarMobile : styles.sideBarDesktop
+            }`}>
+                <Sider>
+                    <Menu mode="inline" className={styles.sidebar} selectedKeys={[currentPath]} inlineCollapsed={isMobile}>
                     {items &&
                         items.map(({ url, name, iconName }) => (
-                            <li key={name}>
+                            <Menu.Item key={url == "" ? "/": url} icon={stringToIcon(iconName)}>
                                 <NavLink
                                     to={`${baseUrl}${url}`}
-                                    activeClassName={styles.active}
-                                >
-                                    {stringToIcon(iconName)}
-                                    <span>{name}</span>
+                                    onClick={(evt) => setCurrentPath(url == "" ? "/": url)}>
+                                    {name}
                                 </NavLink>
-                            </li>
+                            </Menu.Item>
                         ))}
-                </ul>
-            </div>
+                        
+                    </Menu>
+                </Sider>
+
+            </Layout>
         </>
     );
 };
