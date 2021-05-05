e8197f722ba16a3670571e7357db10711f372051
Omer Erdem <omer.erdem@hva.nl>
2021-05-05 17:20:01 +0200

    [WIP] - Merge fix

--- src/services/reservation-service.ts
+++ src/services/reservation-service.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 import IHttpResponse from "./http-response";
 
-import { RESPONSE_OK } from "../assets/constants";
-import { getHttpErrorMessage } from "../assets/functions/error";
-
 import Appointment from "../models/Appointment";
 import User from "../models/User";
 
@@ -23,18 +20,7 @@ export const getCustomerReservations = (): Promise<IReservationResponse> =>
     new Promise<IReservationResponse>((resolve, reject) => {
         axios.get(`${API_URL}/user`).then(
             (response) => {
-                if (response.data.status === RESPONSE_OK) {
-                    resolve(fixUserObject(response.data));
-                } else {
-                    reject(
-                        new Error(
-                            getHttpErrorMessage(
-                                response.data.name,
-                                response.config.url
-                            )
-                        )
-                    );
-                }
+                resolve(fixUserObject(response.data));
             },
             (error) => {
                 reject(new Error(error.message));
@@ -45,8 +31,14 @@ export const getCustomerReservations = (): Promise<IReservationResponse> =>
 // eslint-disable-next-line require-jsdoc
 const fixUserObject = (response: IReservationResponse) => {
     response.data.forEach((value, index) => {
-        response.data[index].customer = User.fromJSON(value.customer);
-        response.data[index].barber = User.fromJSON(value.barber);
+        response.data[index].customer = Object.setPrototypeOf(
+            value.customer,
+            User.prototype
+        );
+        response.data[index].barber = Object.setPrototypeOf(
+            value.barber,
+            User.prototype
+        );
     });
     return response;
 };
