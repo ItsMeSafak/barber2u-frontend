e7b3b19ec6bc6d5965697eb1112bd93737d924b4
Omer Erdem <omer.erdem@hva.nl>
2021-05-25 17:20:29 +0200

    Statistics customer page fetch reviews

--- src/pages/customer/dashboard/statistics/index.tsx
+++ src/pages/customer/dashboard/statistics/index.tsx
@@ -1,12 +1,15 @@
-import React, { useContext } from "react";
-import moment from "moment";
+import React, { useEffect, useState } from "react";
+
 import { Layout } from "antd";
 import { Content } from "antd/es/layout/layout";
+
 import Review from "../../../../models/Review";
-import { AuthenticationContext } from "../../../../contexts/authentication-context";
+
 import WidgetReviews from "../../../../template/widget-reviews";
 
 import CalendarPage from "../../../calendar";
+
+import { fetchReviews } from "../../../../services/review-service";
 
 /**
  * Customer statistics page.
@@ -14,16 +17,14 @@ import CalendarPage from "../../../calendar";
  * @returns {React.FC}
  */
 const StatisticsPage: React.FC = () => {
-    const { user } = useContext(AuthenticationContext);
-    if (user == null) return <h1>OOOPS, Something went wonrg</h1>;
+    const [reviews, setReviews] = useState<Review[]>([]);
 
-    const reviews: Review[] = [
-        new Review("id", user, "image", 5, "review msg5", moment()),
-        new Review("id", user, "image", 4, "review msg4", moment()),
-        new Review("id", user, "image", 3, "review msg3", moment()),
-        new Review("id", user, "image", 2, "review msg2", moment()),
-        new Review("id", user, "image", 1, "review msg1", moment()),
-    ];
+    useEffect(() => {
+        fetchReviews().then((response) =>
+            response.data ? setReviews(response.data) : setReviews([])
+        );
+    }, []);
+
     return (
         <Layout>
             <Content>
